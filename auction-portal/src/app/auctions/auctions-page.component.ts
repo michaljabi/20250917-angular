168c72e2fddf9b75df094b1dec82e978052e9e06
Jabi <michal.jabi@gmail.com>
2025-09-18 09:49:15 +0200

    explain: pipes + example AuctionItem[]

--- auction-portal/src/app/auctions/auctions-page.component.ts
+++ auction-portal/src/app/auctions/auctions-page.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { AuctionItem } from './auction-item';
+import { JsonPipe, UpperCasePipe, LowerCasePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [JsonPipe, UpperCasePipe, LowerCasePipe, CurrencyPipe],
   template: `
-    <p>
-      auctions-page works!
-    </p>
+    <section>
+      <h2>Lista aukcji {{ 200 | currency : 'USD' }}</h2>
+
+      <div>
+        {{ auctions | json | uppercase | lowercase }}
+      </div>
+    </section>
   `,
-  styles: ``
+  styles: ``,
 })
 export class AuctionsPageComponent {
-
+  auctions: AuctionItem[] = [
+    {
+      id: '1',
+      title: 'Części do aparatu',
+      imgUrl: 'https://picsum.photos/id/36/600/600',
+      description: 'Jakiś opis',
+      price: 2000,
+    },
+    {
+      id: '2',
+      title: 'Mac Book',
+      imgUrl: 'https://picsum.photos/id/48/600/600',
+      description: 'Używany - ale sprawny',
+      price: 4000,
+    },
+  ];
 }
