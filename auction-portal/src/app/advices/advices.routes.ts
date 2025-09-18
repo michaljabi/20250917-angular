9cecfb22f7d8b2520a373a715cfdbd7ded081fb0
Jabi <michal.jabi@gmail.com>
2025-09-18 09:30:22 +0200

    refactor(08): advices.routes.ts - separated

---
+++ auction-portal/src/app/advices/advices.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { AdvicesPageComponent } from "./advices-page.component";
+
+export const advicesRoutes: Routes = [
+  { path: 'advices', component: AdvicesPageComponent },
+];
--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { AuctionsPageComponent } from './auctions/auctions-page.component';
-import { AdvicesPageComponent } from './advices/advices-page.component';
 import { PromotionsPageComponent } from './auctions/promotions-page/promotions-page.component';
+import { advicesRoutes } from './advices/advices.routes';
 
 export const routes: Routes = [
   { path: 'auctions', component: AuctionsPageComponent },
   { path: 'promotions', component: PromotionsPageComponent },
-  { path: 'advices', component: AdvicesPageComponent },
+  ...advicesRoutes
 ];
